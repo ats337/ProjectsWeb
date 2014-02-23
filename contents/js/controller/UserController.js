951e1db07a9b8ef5054a2186f62b1853fcde8014
ats337 <ats337@mail.goo.ne.jp>
2014-02-24 01:23:59 +0900

    ユーザ一覧取得処理を追加

--- contents/js/controller/UserController.js
+++ contents/js/controller/UserController.js
@@ -1,5 +1,14 @@
-projectsApp.controller('UserController', 
-    function UserController($scope, $location) {
-    
-    }
+projectsApp.controller('UserController',
+        function UserController($scope, $resource) {
+            $scope.userList = null;
+
+
+            $scope.getUserList = function() {
+                $resource("http://localhost/ProjectsWeb/contents/samplejson/userlist.json").get(
+                        function(data) {
+                            $scope.userList = data.userList;
+                            console.log($scope.userList);
+                        });
+            };
+        }
 );
